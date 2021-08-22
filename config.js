150477ea78d592ece000bfed54d3e0be3ef14310
guoyansai <guoyansai@qq.com>
2021-08-22 14:12:29 +0800

    新增智能机器人

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ var config = {
     broadInRoom: 23,
     broadOutRoom: 24,
     broadMsgFetch: 25,
+    broadMsgAi: 26,
     wsChangeUser: 32,
     wsInRoom: 33,
     wsOutRoom: 34,
   },
-  msgTypeDes: "0消息1发送uid2进入3离开4更新user5广播user",
   userTmp: ["-", 0, "2012-12-12", "-", "-"],
   userTmpDes: ["名字", "头像", "生日", "城市", "签名"],
-  msgTmp: [0, 0, "-", "-"],
-  msgTmpDes: ["用户ID", "消息类型", "消息内容", "时间"],
+  msgTmp: [0, 0, "-", 0, "-"],
+  msgTmpDes: ["用户ID", "消息类型", "消息内容", "说话对象", "时间"],
 };
 try {
   module.exports = config;
